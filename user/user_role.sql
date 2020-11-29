b10b0b210e17de9cd1a89f4e4b51b6c17fd6033a
vt92 <52454990+vt92@users.noreply.github.com>
2020-11-29 14:14:24 +0530

    Create user_role.sql
    added as part of spring security validations

---
+++ user/user_role.sql
@@ -0,0 +1,20 @@
+use reservation
+
+create table role(
+id int not null auto_increment,
+name varchar(20),
+primary key (id)
+)
+
+create table user_role(
+user_id int,
+role_id int,
+foreign key (user_id)
+references user(id),
+foreign key (role_id)
+references role(id),
+)
+
+insert into role values(1,'ADMIN')
+
+insert into user_role values (1,1)
